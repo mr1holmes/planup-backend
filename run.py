82e96d08bc32a44705e2006615b0bfade658ab43
Chirag Chauhan <chirag.holmes@gmail.com>
2016-06-29 19:28:29 +0530

    Setup Heroku

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import flaskapp
+import os
 from flaskapp import app
 
 if __name__ == "__main__":
+    port = int(os.environ.get('PORT',5000))
     #initialize the database before starting the application
     with app.app_context():
         flaskapp.init_db()
-    app.run()
+    app.run(host='0.0.0.0',port=port)
+
